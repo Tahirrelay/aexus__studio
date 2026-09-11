1fb99d49fa9d149b73ba8f96a4addcebf64a7643
Tahirrelay <tahiralone2233@gmail.com>
2026-09-11 16:55:52 +0500

    Fix sitemap domain and update production build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://aexusstudio.com',
+  siteUrl: 'https://aexusstudios.com',
   generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 0.7,
